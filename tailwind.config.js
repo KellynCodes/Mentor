d317e5edf8746077d2a2072640601f388253efd0
kellyncodes <kellyncodes@gmail.com>
2023-12-31 21:43:53 +0100

    tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    fontFamily: {
+      sans: "Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji",
+      inter: "Inter",
+      worksans: "Work Sans",
+      roboto: "Roboto",
+      notosans: "Noto Sans",
+    },
     extend: {
       colors: {
         accent: {
